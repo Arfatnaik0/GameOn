581c0341a9ae30d2b0133603f14e81008e792fbf
Arfat naik <arfatnaik800@gmail.com>
2026-03-14 15:05:06 +0530

    backend start

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,18 @@
 from fastapi import FastAPI
-import pydantic
 from fastapi.middleware.cors import CORSMiddleware
+import os
+import dotenv
+from routers import games,users
+
+app.include_router(games.router)
+app.include_router(users.router)
+
+dotenv.load_dotenv()
+
+project_url = os.getenv("SUPABASE_URL")
+anon_key = os.getenv("SUPABASE_ANON_KEY")
+JWT_key = os.getenv("SUPABASE_JWT_SECRET")
+rawg_api_key = os.getenv("RAWG_API_KEY")
 
 app = FastAPI()
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_methods=["*"], allow_headers=["*"])
